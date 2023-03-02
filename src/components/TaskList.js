0d5fa36ff4f4847fb11f49b9ad9d0166260531be
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-02 22:49:18 +0630

    create TaskCard with props

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { TaskCard } from "./TaskCard";
 
 export const TaskList = () => {
   const [tasks, setTasks] = useState([
@@ -21,17 +22,7 @@ export const TaskList = () => {
         </button>
         {show &&
           tasks.map((task) => (
-            <li
-              key={task.id}
-              className={task.completed ? "completed" : "incomplete"}
-            >
-              <span>
-                {task.id} - {task.name}
-              </span>
-              <button onClick={() => handleDelete(task.id)} className="delete">
-                Delete
-              </button>
-            </li>
+            <TaskCard key={task.id} task={task} handleDelete={handleDelete} />
           ))}
       </ul>
     </>
