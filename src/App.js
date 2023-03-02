a2c91b831cd1859b75599b8f296ea7cd228773a2
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-02 20:19:39 +0630

    create header and footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 import "./App.css";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>Shubham</h1>
+      </div>
+      <Footer />
+    </>
+  );
 }
