61c89973d94980560b8acd1e8101a9df35ac51b9
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-03 19:33:21 +0630

    create BoxCard

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { TaskCard } from "./TaskCard";
+import { BoxCard } from "./BoxCard";
 
 export const TaskList = () => {
   const [tasks, setTasks] = useState([
@@ -25,6 +26,29 @@ export const TaskList = () => {
             <TaskCard key={task.id} task={task} handleDelete={handleDelete} />
           ))}
       </ul>
+      <BoxCard result="success">
+        <p className="title">Lorem ipsum dolor sit amet.</p>
+        <p className="description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
+          vel!
+        </p>
+      </BoxCard>
+
+      <BoxCard result="warning">
+        <p className="title">Lorem ipsum dolor sit.</p>
+        <p className="description">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, eum
+          odit! Labore eaque fuga repellat adipisci molestias quasi animi fugiat
+          necessitatibus sunt vel, optio rem non quidem! Blanditiis cupiditate
+          iusto omnis reprehenderit assumenda maxime, nam perferendis impedit
+          libero odit eius eum aut cum ad, excepturi officiis repudiandae.
+          Molestiae, eum cumque?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+          nulla?
+        </p>
+      </BoxCard>
     </>
   );
 };
