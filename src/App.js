9532be1313a65be3e3909aacdb88ad26c979bd57
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-04 10:45:52 +0630

    Controlled Inputs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { TaskList } from "./components/TaskList";
-import "./App.css";
+import { AddTask } from "./components/AddTask";
+
+import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <Header />
+      <AddTask />
       <TaskList />
       <Footer />
     </div>
