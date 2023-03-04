17a88d74be1431481c06942b75efd518bee19794
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-04 16:27:04 +0630

    update task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,56 +1,20 @@
-import { useState, useRef } from "react";
-import "./AddTask.css";
-
-export const AddTask = ({ tasks, setTasks }) => {
-  // const [taskValue, setTaskValue] = useState("");
-  const [progress, setProgress] = useState(false);
-  const taskRef = useRef("");
-
-  // const handleChange = (event) => {
-  //     console.log(taskRef.current.value)
-  // }
-
-  const handleReset = () => {
-    // setTaskValue("");
-    taskRef.current.value = "";
-    setProgress(false);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const task = {
-      id: Math.floor(Math.random() * 10000),
-      name: taskRef.current.value,
-      completed: Boolean(progress),
-    };
-    setTasks([...tasks, task]);
-    handleReset();
-  };
-
+const AddTask = ({ handleSubmit, editid, task, setTask }) => {
   return (
-    <section className="addtask">
+    <section className="addTask">
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="task"
-          id="task"
-          placeholder="Task Name"
+          value={task}
           autoComplete="off"
-          ref={taskRef}
+          placeholder="add task"
+          maxLength="25"
+          onChange={(e) => setTask(e.target.value)}
         />
-        <select
-          onChange={(event) => setProgress(event.target.value)}
-          value={progress}
-        >
-          <option value="false">Pending</option>
-          <option value="true">Completed</option>
-        </select>
-        <span onClick={handleReset} className="reset">
-          Reset
-        </span>
-        <button type="submit">Add Task</button>
+        <button type="submit">{editid ? "Update" : "Add"}</button>
       </form>
-      <p>{taskRef.current.value}</p>
     </section>
   );
 };
+
+export default AddTask;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,48 @@
-import Logo from "../assets/logo.png";
-import "./Header.css";
+import logo from "../assets/logo.svg";
 
-export const Header = () => {
+const Header = ({ children, theme, setTheme }) => {
   return (
     <header>
-      <img src={Logo} alt="" />
-      <a href="/">Home</a>
+      <span className="logo">
+        <img src={logo} alt="" />
+        <span>{children}</span>
+      </span>
+      <span className="themeSelector">
+        <span
+          className={theme === "light" ? "light activeTheme" : "light"}
+          onClick={() => setTheme("light")}
+        ></span>
+        <span
+          className={theme === "medium" ? "medium activeTheme" : "medium"}
+          onClick={() => setTheme("medium")}
+        ></span>
+        <span
+          className={theme === "dark" ? "dark activeTheme" : "dark"}
+          onClick={() => setTheme("dark")}
+        ></span>
+        <span
+          className={
+            theme === "gradientOne" ? "gradientOne activeTheme" : "gradientOne"
+          }
+          onClick={() => setTheme("gradientOne")}
+        ></span>
+        <span
+          className={
+            theme === "gradientTwo" ? "gradientTwo activeTheme" : "gradientTwo"
+          }
+          onClick={() => setTheme("gradientTwo")}
+        ></span>
+        <span
+          className={
+            theme === "gradientThree"
+              ? "gradientThree activeTheme"
+              : "gradientThree"
+          }
+          onClick={() => setTheme("gradientThree")}
+        ></span>
+      </span>
     </header>
   );
 };
+
+export default Header;
