ed769492cfccc5e102ccab2c7d8df863e2143d8a
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-04 10:15:46 +0630

    styling

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { TaskCard } from "./TaskCard";
-import { BoxCard } from "./BoxCard";
 import "./TaskList.css";
+import "./AddTask.css";
 
 export const TaskList = () => {
   const [tasks, setTasks] = useState([
@@ -17,31 +17,18 @@ export const TaskList = () => {
 
   return (
     <section className="tasklist">
-      <h1>Task List</h1>
       <ul>
-        <button className="trigger" onClick={() => setShow(!show)}>
-          Toggle
-        </button>
+        <div className="header">
+          <h1>TaskList</h1>
+          <button className="trigger" onClick={() => setShow(!show)}>
+            {show ? "Hide Tasks" : "Show Tasks"}
+          </button>
+        </div>
         {show &&
           tasks.map((task) => (
             <TaskCard key={task.id} task={task} handleDelete={handleDelete} />
           ))}
       </ul>
-      <BoxCard result="success">
-        <p className="title">Offer Notification</p>
-        <p className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis,
-          vel!
-        </p>
-      </BoxCard>
-
-      <BoxCard result="warning">
-        <p className="title">Cookie Notification</p>
-        <p className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
-          suscipit.
-        </p>
-      </BoxCard>
     </section>
   );
 };
